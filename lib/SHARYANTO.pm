4f5f9fb47e4d1c8b0142cabf9b2eee9c254af085
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-04-12 18:45:05 +0700

    Initial commit

---
+++ lib/SHARYANTO.pm
@@ -0,0 +1,21 @@
+package SHARYANTO;
+
+# VERSION
+
+1;
+# ABSTRACT: SHARYANTO's temporary namespace for various libraries
+
+=head1 FAQ
+
+=head2 What is this? Why SHARYANTO::*?
+
+The SHARYANTO namespace is used by me for B<temporary> placeholder for modules
+that are either alpha quality, unstable, lacking tests and/or documentation, or
+that haven't found a proper name yet.
+
+I put such modules on CPAN because some of my other modules (and scripts) depend
+on it.
+
+To reiterate, this namespace is for temporary placeholder and not for vanity :)
+
+=cut
